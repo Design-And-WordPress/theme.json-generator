5190297eaf20697b8f56a9206638162340dc1657
mimi <mimitips@gmail.com>
2023-05-18 21:52:06 +0900

    feature/output json color data

--- code.ts
+++ code.ts
@@ -1,15 +1,67 @@
+type colorType = {
+  color: string;
+  name: string;
+  slug: string;
+};
+
 figma.showUI(__html__);
-figma.ui.resize(800, 800);
+figma.ui.resize(500, 400);
 
 // 現在のファイルの塗りのスタイルを取得
 const styles = figma.getLocalPaintStyles();
 
-if (figma.editorType === "figma") {
-  figma.ui.onmessage = (message) => {
-    console.log(message);
-    let currentData;
-    currentData = styles;
+// 255に換算
+const to255 = (rgb: number) => {
+  return Math.round(rgb * 255);
+};
 
-    figma.ui.postMessage({ type: "render", body: currentData });
-  };
+// RGBA換算
+const toRgba = (rgb: RGB, opacity: number) => {
+  return (
+    to255(rgb.r) +
+    "," +
+    to255(rgb.g) +
+    "," +
+    to255(rgb.b) +
+    "," +
+    opacity.toFixed(1)
+  );
+};
+
+// toCamelCase
+const camelize = (str: string) => {
+  const wordsArr = str.split(/[\W-_]/g);
+  const camelizedWords = wordsArr
+    .map((word) => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    })
+    .join("");
+  return camelizedWords;
+};
+
+// toSnakeCase
+const snakeCase = (str: string) => {
+  const camelizedWords = camelize(str);
+  return camelizedWords.replace(/[A-Z]/g, (c) => { return '_' + c.toLowerCase() }).slice(1);
+};
+
+if (figma.editorType === "figma") {
+
+  const colors = styles.map((style) => {
+    if (style.paints[0].type !== "SOLID") {
+      // 単色でなければスキップ
+      return;
+    }
+    
+    const color = style.paints[0].color;
+    const slug = snakeCase(style.name);
+    const name = snakeCase(style.name);
+    const colorData:colorType = {
+      name: name,
+      slug: slug,
+      color: 'rgba(' + toRgba(color, Number(style.paints[0].opacity)) + ')'
+    }
+    return JSON.stringify(colorData);
+  }).join(",");
+  figma.ui.postMessage({ type: "render", body: colors });
 }
